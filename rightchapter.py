7926cfa79538cf86e2a5ff9fb3448055fb04a726
unicornlaunching <45397093+unicornlaunching@users.noreply.github.com>
2023-12-08 18:27:03 -0500

    Update rightchapter.py

--- rightchapter.py
+++ rightchapter.py
@@ -1,15 +1,29 @@
 import streamlit as st
-from bokeh.models.widgets import Button
+from bokeh.models.widgets import Button, Div
+from bokeh.layouts import column
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
 
+# Create the Speak button and timer display
 stt_button = Button(label="Speak", width=100)
+timer_display = Div(text="Timer: 120")
 
-stt_button.js_on_event("button_click", CustomJS(code="""
+# JavaScript code for the button click event
+stt_button.js_on_event("button_click", CustomJS(args=dict(timer_display=timer_display), code="""
     var recognition = new webkitSpeechRecognition();
     recognition.continuous = true;
     recognition.interimResults = true;
- 
+
+    var timeLeft = 120;
+    var timerInterval = setInterval(function() {
+        timeLeft--;
+        timer_display.text = 'Timer: ' + timeLeft;
+        if (timeLeft <= 0) {
+            clearInterval(timerInterval);
+            recognition.stop();
+        }
+    }, 1000);
+    
     recognition.onresult = function (e) {
         var value = "";
         for (var i = e.resultIndex; i < e.results.length; ++i) {
@@ -21,15 +35,24 @@ stt_button.js_on_event("button_click", CustomJS(code="""
             document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
         }
     }
+
     recognition.start();
+
+    recognition.onend = function() {
+        clearInterval(timerInterval);
+    }
     """))
 
+# Layout for the button and timer display
+layout = column(stt_button, timer_display)
+
+# Event handling in Streamlit
 result = streamlit_bokeh_events(
-    stt_button,
+    layout,
     events="GET_TEXT",
     key="listen",
     refresh_on_update=False,
-    override_height=75,
+    override_height=200,
     debounce_time=0)
 
 if result:
